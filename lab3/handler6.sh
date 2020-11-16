e2de83e32a24d19a5710df142b74955bd7c7ef36
Zhanna <58556862+zhannarui@users.noreply.github.com>
2020-11-16 23:44:18 +0300

    create task6

---
+++ lab3/handler6.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+echo $$ > pipe6 
+num=1
+command="+"
+
+sigterm()
+{
+     echo "exit"
+     exit
+}
+
+user1()
+{
+     command="+"
+}
+
+user2()
+{
+command="*"
+}
+
+trap 'sigterm' SIGTERM
+trap 'user1' USR1
+trap 'user2' USR2
+
+while true
+do 
+      case $command in 
+      "+")
+          let num=$num+2
+          echo $num
+      ;;
+      "*")
+          let num=$num*2
+          echo $num
+      ;;
+      esac
+sleep 2
+done
+      
